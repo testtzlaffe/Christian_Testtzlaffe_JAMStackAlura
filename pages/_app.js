c1a9eeaf6ed7c1461720e50f9270c260fdb2878e
Christian Alpoim <chrufes@yahoo.com.br>
2021-02-20 16:49:32 -0300

    Retirar charset do head

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,6 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Portfolio</title>
